45b19cb7124928f2fccf63fdfa5ee199135610bb
Sergei Strizhenok <43453824+pro100star@users.noreply.github.com>
2019-04-17 00:30:17 +0300

    Add files via upload

--- Visualization.Designer.cs
+++ Visualization.Designer.cs
@@ -23,7 +23,10 @@
         /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
             this.button_start = new System.Windows.Forms.Button();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.GraphData = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button_start
@@ -34,13 +37,36 @@
             this.button_start.TabIndex = 0;
             this.button_start.Text = "Старт\r\n";
             this.button_start.UseVisualStyleBackColor = true;
-            this.button_start.Click += new System.EventHandler(this.start_button);
+            this.button_start.Click += new System.EventHandler(this.Start_button);
+            // 
+            // zedGraph
+            // 
+            this.zedGraph.Location = new System.Drawing.Point(0, 0);
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.ScrollGrace = 0D;
+            this.zedGraph.ScrollMaxX = 0D;
+            this.zedGraph.ScrollMaxY = 0D;
+            this.zedGraph.ScrollMaxY2 = 0D;
+            this.zedGraph.ScrollMinX = 0D;
+            this.zedGraph.ScrollMinY = 0D;
+            this.zedGraph.ScrollMinY2 = 0D;
+            this.zedGraph.Size = new System.Drawing.Size(150, 150);
+            this.zedGraph.TabIndex = 0;
+            // 
+            // GraphData
+            // 
+            this.GraphData.FormattingEnabled = true;
+            this.GraphData.Location = new System.Drawing.Point(125, 47);
+            this.GraphData.Name = "GraphData";
+            this.GraphData.Size = new System.Drawing.Size(204, 160);
+            this.GraphData.TabIndex = 1;
             // 
             // Visualization
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(442, 288);
+            this.Controls.Add(this.GraphData);
             this.Controls.Add(this.button_start);
             this.Name = "Visualization";
             this.Text = "Graph Research";
@@ -50,7 +76,9 @@
 
         #endregion
 
+        private ZedGraph.ZedGraphControl zedGraph;
         private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.ListBox GraphData;
     }
 }
 
--- Visualization.cs
+++ Visualization.cs
@@ -1,21 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZedGraph;
 
 namespace WF {
     public partial class Visualization : Form {
         public Visualization() {
             InitializeComponent();
+            GraphData.Visible = false;
         }
 
-        private void start_button(object sender, EventArgs e) {
+        private void Start_button(object sender, EventArgs e) {
             button_start.Visible = false;
+            GraphData.Visible = true;
+            Controls.Add(zedGraph);
+            InitializeZedGraph();
+
+            List<List<Pair<int, double>>> data = new List<List<Pair<int, double>>>();
+            data.Add(new List<Pair<int, double>>());
+            data[0].Add(new Pair<int, double>(0, Math.Sqrt(2)));
+            data[0].Add(new Pair<int, double>(0, Math.Sqrt(3)));
+            data[0].Add(new Pair<int, double>(0, Math.Sqrt(5)));
+            Graph graph = new Graph(data, 1, 3);
+            DrawGraph(graph, 10, 1);
+        }
+
+        private void ReadGraph() {
+            //string 
+        }
+
+        private void InitializeZedGraph() {
+            zedGraph.Dock = DockStyle.Fill;
+            zedGraph.Location = new Point(0, 0);
+            zedGraph.Name = "zedGraph";
+            zedGraph.ScrollGrace = 0D;
+            zedGraph.ScrollMaxX = 0D;
+            zedGraph.ScrollMaxY = 0D;
+            zedGraph.ScrollMaxY2 = 0D;
+            zedGraph.ScrollMinX = 0D;
+            zedGraph.ScrollMinY = 0D;
+            zedGraph.ScrollMinY2 = 0D;
+            zedGraph.Size = new Size(783, 471);
+            zedGraph.TabIndex = 0;
+        }
+
+        private void DrawGraph(Graph graph, int t, int m) {
+            GraphPane graphPane = zedGraph.GraphPane;
+            graphPane.CurveList.Clear();
+
+            double[] count = Array.ConvertAll(graph.Research(t, m), (int value) => (double)value);
+            //double[] time = new double[count.Length];
+            PointPairList points = new PointPairList();
+            //for (int i = 0; i < 100; ++i) points.Add(i, i);
+            int counter = 0;
+            for (double i = 0; i * 100 < count.Length && counter < count.Length; i += 0.01, ++counter) {
+                points.Add(i, count[counter]);
+            }
+            LineItem graphic = graphPane.AddCurve("Sinc", points, Color.Blue, SymbolType.None);
+            zedGraph.AxisChange();
+            zedGraph.Invalidate();
         }
     }
 }
